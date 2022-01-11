c67a2f938ae6b1b7a88209c3d096b3b85298eb3e
Joel Grant <joelmgrant@me.com>
2022-01-11 11:08:51 -0700

    Test: Create test for order of top 5 merchants

--- spec/features/admin/merchants/index_spec.rb
+++ spec/features/admin/merchants/index_spec.rb
@@ -5,6 +5,33 @@ RSpec.describe 'admin merchants index' do
   let!(:merchant_2) {FactoryBot.create(:merchant, status: "enabled")}
   let!(:merchant_3) {FactoryBot.create(:merchant, status: "enabled")}
   let!(:merchant_4) {FactoryBot.create(:merchant, status: "disabled")}
+  let!(:merchant_5) {FactoryBot.create(:merchant)}
+  let!(:merchant_6) {FactoryBot.create(:merchant)}
+  let!(:merchant_7) {FactoryBot.create(:merchant)}
+
+  let!(:item_1) {FactoryBot.create(:item, merchant_id: merchant_1.id)}
+  let!(:item_2) {FactoryBot.create(:item, merchant_id: merchant_2.id)}
+  let!(:item_3) {FactoryBot.create(:item, merchant_id: merchant_3.id)}
+  let!(:item_4) {FactoryBot.create(:item, merchant_id: merchant_4.id)}
+  let!(:item_5) {FactoryBot.create(:item, merchant_id: merchant_5.id)}
+  let!(:item_6) {FactoryBot.create(:item, merchant_id: merchant_6.id)}
+  let!(:item_7) {FactoryBot.create(:item, merchant_id: merchant_7.id)}
+
+  let!(:transaction_1) {FactoryBot.create(:transaction, result: "success")}
+  let!(:transaction_2) {FactoryBot.create(:transaction, result: "success")}
+  let!(:transaction_3) {FactoryBot.create(:transaction, result: "success")}
+  let!(:transaction_4) {FactoryBot.create(:transaction, result: "success")}
+  let!(:transaction_5) {FactoryBot.create(:transaction, result: "success")}
+  let!(:transaction_6) {FactoryBot.create(:transaction, result: "success")}
+  let!(:transaction_7) {FactoryBot.create(:transaction, result: "success")}
+
+  let!(:invoice_item_1) {FactoryBot.create(:invoice_item, invoice_id: transaction_1.invoice.id, item_id: item_1.id, quantity: 1, unit_price: 10)}
+  let!(:invoice_item_2) {FactoryBot.create(:invoice_item, invoice_id: transaction_2.invoice.id, item_id: item_2.id, quantity: 2, unit_price: 10)}
+  let!(:invoice_item_3) {FactoryBot.create(:invoice_item, invoice_id: transaction_3.invoice.id, item_id: item_3.id, quantity: 3, unit_price: 10)}
+  let!(:invoice_item_4) {FactoryBot.create(:invoice_item, invoice_id: transaction_4.invoice.id, item_id: item_4.id, quantity: 4, unit_price: 10)}
+  let!(:invoice_item_5) {FactoryBot.create(:invoice_item, invoice_id: transaction_5.invoice.id, item_id: item_5.id, quantity: 5, unit_price: 10)}
+  let!(:invoice_item_6) {FactoryBot.create(:invoice_item, invoice_id: transaction_6.invoice.id, item_id: item_6.id, quantity: 6, unit_price: 10)}
+  let!(:invoice_item_7) {FactoryBot.create(:invoice_item, invoice_id: transaction_7.invoice.id, item_id: item_7.id, quantity: 7, unit_price: 10)}
   before(:each) do
     visit '/admin/merchants'
   end
@@ -69,23 +96,17 @@ RSpec.describe 'admin merchants index' do
   end
 
   describe 'Top 5 Merchants Section' do
-    let!(:invoice_item_1) {FactoryBot.create(:invoice_item, quantity: 1, unit_price: 10)}
-    let!(:invoice_item_2) {FactoryBot.create(:invoice_item, quantity: 2, unit_price: 10)}
-    let!(:invoice_item_3) {FactoryBot.create(:invoice_item, quantity: 3, unit_price: 10)}
-    let!(:invoice_item_4) {FactoryBot.create(:invoice_item, quantity: 4, unit_price: 10)}
-    let!(:invoice_item_5) {FactoryBot.create(:invoice_item, quantity: 5, unit_price: 10)}
-    let!(:invoice_item_6) {FactoryBot.create(:invoice_item, quantity: 6, unit_price: 10)}
-    let!(:invoice_item_7) {FactoryBot.create(:invoice_item, quantity: 7, unit_price: 10)}
-
     it 'has a section for the top five merchants by total revenue' do
       expect(page).to have_content("Top Five Merchants By Revenue")
     end
 
     it 'has the names of each of the top 5 merchants' do
-      expect(invoice_item_7.item.merchant).to appear_before(invoice_item_6.item.merchant)
-      expect(invoice_item_6.item.merchant).to appear_before(invoice_item_5.item.merchant)
-      expect(invoice_item_5.item.merchant).to appear_before(invoice_item_4.item.merchant)
-      expect(invoice_item_4.item.merchant).to appear_before(invoice_item_3.item.merchant)
+      within "#top-five-merchants" do
+        expect(invoice_item_7.item.merchant.name).to appear_before(invoice_item_6.item.merchant.name)
+        expect(invoice_item_6.item.merchant.name).to appear_before(invoice_item_5.item.merchant.name)
+        expect(invoice_item_5.item.merchant.name).to appear_before(invoice_item_4.item.merchant.name)
+        expect(invoice_item_4.item.merchant.name).to appear_before(invoice_item_3.item.merchant.name)
+    end
     end
   end
 end
