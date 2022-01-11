f01fa5128a90e63f82c6d023279cae643009e25a
Kerri Hoffmann <kerri.hoffmann@gmail.com>
2022-01-11 15:53:47 -0700

    add additional tests to account for partially filled out new item form

--- spec/features/merchants/items/new_spec.rb
+++ spec/features/merchants/items/new_spec.rb
@@ -35,19 +35,55 @@ RSpec.describe "New Item Creation" do
     expect(new_item.status).to eq("disabled")
   end
 
-  it 'shows the user an error if the form is not filled out correctly' do
-    merchant_1 = Merchant.create!(name: "Clothing Store")
-    item_1 = merchant_1.items.create!(name: "Sweater", description: "Red Sweater", unit_price: 40)
-    item_2 = merchant_1.items.create!(name: "Hat", description: "Beanie", unit_price: 20, status: 1)
-    item_3 = merchant_1.items.create!(name: "Shoes", description: "Running Shoes", unit_price: 80)
+  describe 'error message for incomplete new item form' do
+    before(:each) do
+      @merchant_1 = Merchant.create!(name: "Clothing Store")
+      visit "/merchants/#{@merchant_1.id}/items/new"
+    end
 
-    visit "/merchants/#{merchant_1.id}/items/new"
+    it 'shows the user an error if the new form is not filled out with anything' do
+      click_on("Submit")
 
-    click_on("Submit")
+      expect(current_path).to eq("/merchants/#{@merchant_1.id}/items")
 
-    expect(current_path).to eq("/merchants/#{merchant_1.id}/items")
-    
-    expect(page).to have_content("Item not created")
-    expect(page).to have_link("Add New Item")
+      expect(page).to have_content("Item not created")
+      expect(page).to have_link("Add New Item")
+    end
+
+    it 'shows the user an error if the new form is not filled out with the name' do
+      fill_in('Description' , with: "Blue T-shirt with Logo")
+      fill_in('Unit price', with: 25)
+
+      click_on("Submit")
+
+      expect(current_path).to eq("/merchants/#{@merchant_1.id}/items")
+
+      expect(page).to have_content("Item not created")
+      expect(page).to have_link("Add New Item")
+    end
+
+    it 'shows the user an error if the new form is not filled out with the description' do
+      fill_in('Name' , with: "Sweet T-Shirt")
+      fill_in('Unit price', with: 25)
+
+      click_on("Submit")
+
+      expect(current_path).to eq("/merchants/#{@merchant_1.id}/items")
+
+      expect(page).to have_content("Item not created")
+      expect(page).to have_link("Add New Item")
+    end
+
+    it 'shows the user an error if the new form is not filled out with the unit price' do
+      fill_in('Name' , with: "Sweet T-Shirt")
+      fill_in('Description' , with: "Blue T-shirt with Logo")
+
+      click_on("Submit")
+
+      expect(current_path).to eq("/merchants/#{@merchant_1.id}/items")
+
+      expect(page).to have_content("Item not created")
+      expect(page).to have_link("Add New Item")
+    end
   end
 end
