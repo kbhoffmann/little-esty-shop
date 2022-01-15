3e0b579712cb86949f8ca41f0a443b0827412b79
Parker Lockhartn <parkerlockhart42@gmail.comm>
2022-01-15 17:40:30 -0600

    Add facade to view

--- app/controllers/merchant_discounts_controller.rb
+++ app/controllers/merchant_discounts_controller.rb
@@ -1,9 +1,10 @@
 class MerchantDiscountsController < ApplicationController
   def index
+    @facade = MerchantFacade.new
     @merchant = Merchant.find(params[:merchant_id])
   end
 
   def show
     @discount = Discount.find(params[:discount_id])
   end
-end 
+end
