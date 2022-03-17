756cd100494b2630eae947d2b4d3ef76eb3b97c4
Parker Lockhart <parkerlockhart42@gmail.comm>
2022-03-17 15:15:26 -0500

    Add welcome page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
+  root to: 'welcome#index'
+  get '/register', to: 'users#new'
+  get '/login', to: 'sessions#create'
+  get '/logout', to: 'sessions#destroy'
+  
   resources :merchants, only: [:show] do
     resources :dashboard, only: [:index]
     resources :items, except: [:destroy]
---
+++ spec/features/welcome/index_spec.rb
@@ -0,0 +1,4 @@
+require 'rails_helper'
+
+RSpec.describe 'Welcome Page' do
+end 
